61703b96b3990972427181671640e5d09610c871
ethan <ethanye@foxmail.com>
2018-06-12 14:03:40 +0800

    Formatted build scripts for 'hexagon-core'.

--- hexagon-core/src/androidTest/java/com/honeycomb/hexagon/ExampleInstrumentedTest.java
+++ hexagon-core/src/androidTest/java/com/honeycomb/hexagon/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.honeycomb.hexagon", appContext.getPackageName());
+        assertEquals("com.honeycomb.hexagon.test", appContext.getPackageName());
     }
 }
